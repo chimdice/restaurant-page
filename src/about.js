ba5b19ee06ebc9d01f4bf527775622028f4d924c
chimdice <118031439+chimdice@users.noreply.github.com>
2024-05-31 19:12:06 -0700

    Finish about tab

--- src/about.js
+++ src/about.js
@@ -2,6 +2,24 @@ const loadAboutPage = () => {
     const middle = document.createElement('div');
     middle.classList.add("middle");
 
+    const about = document.createElement("div");
+    about.id = "about";
+
+    const para = document.createElement("p");
+    para.textContent = `
+        Hello, this is the Jal clans offical pie shop. We are situtated in a small city of Jalcity.
+        We sell a wide variety of pies that will sure;y statisfy your needs
+    `;
+    about.appendChild(para);
+
+    const para2 = document.createElement("p");
+    para2.textContent = `
+        Email: Jalclan@jalmail.com
+    `;
+    about.appendChild(para2);
+
+    middle.appendChild(about);
+
     return middle;
 };
 
