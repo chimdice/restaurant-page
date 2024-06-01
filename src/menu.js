3dfc8f2be92030f49a7bbf8061825cc4f0e7525b
chimdice <118031439+chimdice@users.noreply.github.com>
2024-05-31 18:59:38 -0700

    Finish the menu tab

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,18 @@ const loadMenuPage = () => {
     const middle = document.createElement('div');
     middle.classList.add("middle");
 
+    const menu = document.createElement('ul');
+    menu.id = "menu"
+
+    const pies = {apple: "$5", cherry:"$5", pumpkin:"$5", peach:"$5"}
+
+    for (let pie in pies) {
+        const entry = document.createElement("li");
+        entry.textContent = pie + " - " + pies[pie];
+        menu.appendChild(entry);
+    };
+
+    middle.appendChild(menu);
     return middle;
 };
 
